94d4a0e0b9e138c9c105d1fab95769342cb159b1
Chris <raoxc@mogulinker.com>
2020-10-15 22:23:13 +0800

    getInitialProps方法内使用axios请求数据

--- pages/user.js
+++ pages/user.js
@@ -2,16 +2,17 @@
  * @Author: Chris
  * @Date: 2020-10-15 19:32:40
  * @LastEditors: Chris
- * @LastEditTime: 2020-10-15 22:01:03
+ * @LastEditTime: 2020-10-15 22:22:18
  * @Descripttion: **
  */
 import React, { useState } from 'react';
 import Layout from '../components/Layout'
 import dynamic from 'next/dynamic'
+import axios from 'axios'
 // import One from '../components/One'
 const One = dynamic(import('../components/One')) // 懒加载组件(需要显示时才加载)
-
-export default () => {
+const User = ({id}) => {
+  console.log(id)
   const [time, setTime] = useState(Date.now())
   const [visible, setVisible] = useState(false);
   const changeTime = async () => {
@@ -26,3 +27,13 @@ export default () => {
     <button onClick={changeTime}>改变时间格式</button>
   </Layout>)
 }
+// 异步处理
+User.getInitialProps = async () => {
+  const result = new Promise(resolve => {
+    axios.get('http://52.81.146.173:60014/api/swagger.json').then(res => {
+      resolve({ id: 1 })
+    })
+  })
+  return await result;
+}
+export default User;
