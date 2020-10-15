01e92b5602ba15e818f2fcba77495383f7b7cfe9
Chris <raoxc@mogulinker.com>
2020-10-15 22:34:09 +0800

    style jsx 样式

--- pages/user.js
+++ pages/user.js
@@ -2,7 +2,7 @@
  * @Author: Chris
  * @Date: 2020-10-15 19:32:40
  * @LastEditors: Chris
- * @LastEditTime: 2020-10-15 22:22:18
+ * @LastEditTime: 2020-10-15 22:29:28
  * @Descripttion: **
  */
 import React, { useState } from 'react';
@@ -15,25 +15,38 @@ const User = ({id}) => {
   console.log(id)
   const [time, setTime] = useState(Date.now())
   const [visible, setVisible] = useState(false);
+  const [color, setColor] = useState('red');
   const changeTime = async () => {
     const moment = await import('moment');// 懒加载模块
     setTime(moment.default().format());
     setVisible(true)
+    setColor('blue')
   }
   return (<Layout>
     这是个人中心页
     {visible&&<One />}
     <div>time: {time}</div>
     <button onClick={changeTime}>改变时间格式</button>
+    <div style={{color: 'green'}}>行内样式</div>
+    <style jsx>
+      {
+        `
+        div{
+          color: ${color}
+        }
+        `
+      }
+    </style>
   </Layout>)
 }
 // 异步处理
 User.getInitialProps = async () => {
-  const result = new Promise(resolve => {
-    axios.get('http://52.81.146.173:60014/api/swagger.json').then(res => {
-      resolve({ id: 1 })
-    })
-  })
-  return await result;
+  // const result = new Promise(resolve => {
+  //   axios.get('http://52.81.146.173:60014/api/swagger.json').then(res => {
+  //     resolve({ id: 1 })
+  //   })
+  // })
+  // return await result;
+  return {}
 }
 export default User;
