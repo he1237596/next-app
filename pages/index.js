51ef23e84438d9d92399d5e20929973a938feff4
Chris <raoxc@mogulinker.com>
2020-10-15 22:16:23 +0800

    路由钩子事件

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,27 @@
  * @Author: Chris
  * @Date: 2020-10-15 17:55:43
  * @LastEditors: Chris
- * @LastEditTime: 2020-10-15 20:48:24
+ * @LastEditTime: 2020-10-15 22:10:04
  * @Descripttion: **
  */
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import Router from 'next/router'
-
+Router.events.on('routeChangeStart',(...args)=>{
+  console.log(`routeChangeStart:`,args);
+})
+Router.events.on('beforeHistoryChange',(...args)=>{
+  console.log(`beforeHistoryChange:`,args);
+})
+Router.events.on('routeChangeComplete',(...args)=>{
+  console.log(`routeChangeComplete:`,args);
+})
+Router.events.on('hashChangeStart',(...args)=>{
+  console.log(`routeChangeStart:`,args);
+})
+Router.events.on('hashChangeComplete',(...args)=>{
+  console.log(`routeChangeComplete:`,args);
+})
 export default function Home() {
   const jump = (pathname) => {
     Router.push({pathname, query: {id: 1}}) // 传参
@@ -35,6 +49,7 @@ export default function Home() {
         <button onClick={() => jump('/goods/detail')}>Router去详情页</button>
       </main>
       <p>ggggg</p>
+      <Link href='#test'><a>测试hash</a></Link>
     </div>
   )
 }
