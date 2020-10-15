f613ec995b4d67b97dfbb6e7cecdb1f028135530
Chris <raoxc@mogulinker.com>
2020-10-15 22:01:51 +0800

    懒加载

---
+++ components/One.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: Chris
+ * @Date: 2020-10-15 21:32:05
+ * @LastEditors: Chris
+ * @LastEditTime: 2020-10-15 21:32:29
+ * @Descripttion: **
+ */
+export default () => <div>懒加载测试组件</div>
--- pages/user.js
+++ pages/user.js
@@ -2,13 +2,27 @@
  * @Author: Chris
  * @Date: 2020-10-15 19:32:40
  * @LastEditors: Chris
- * @LastEditTime: 2020-10-15 19:53:09
+ * @LastEditTime: 2020-10-15 22:01:03
  * @Descripttion: **
  */
+import React, { useState } from 'react';
 import Layout from '../components/Layout'
-export default () => {
+import dynamic from 'next/dynamic'
+// import One from '../components/One'
+const One = dynamic(import('../components/One')) // 懒加载组件(需要显示时才加载)
 
+export default () => {
+  const [time, setTime] = useState(Date.now())
+  const [visible, setVisible] = useState(false);
+  const changeTime = async () => {
+    const moment = await import('moment');// 懒加载模块
+    setTime(moment.default().format());
+    setVisible(true)
+  }
   return (<Layout>
     这是个人中心页
+    {visible&&<One />}
+    <div>time: {time}</div>
+    <button onClick={changeTime}>改变时间格式</button>
   </Layout>)
 }
